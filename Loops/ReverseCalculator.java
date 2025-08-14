a2c7acb73326434dbfcf7ffb0f42d57f92c35e76
Tejas Tp <tejastp193@gmail.com>
2025-08-14 10:02:07 +0530

    Add files via upload

---
+++ Loops/ReverseCalculator.java
@@ -0,0 +1,22 @@
+package Loops;
+import java.util.Scanner;
+
+public class ReverseCalculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		int reversed = 0;
+		
+		while(n != 0) {
+			int digit = n % 10;
+			reversed = reversed * 10 + digit;
+			n = n/10;
+		}
+		
+		System.out.println("Reversed number: "+ reversed);
+		sc.close();
+	}
+
+}
